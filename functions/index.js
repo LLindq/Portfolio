5e1b23b7d44fc286b270bd8cbf8526ddf07bc847
LLindq <leoniversum@hotmail.com>
2020-12-15 14:48:28 +0100

    Firebase progresion, signup / email / users

--- functions/index.js
+++ functions/index.js
@@ -11,6 +11,33 @@ const {
     // ..,
     postOneTodo
 } = require('./APIs/todos')
+
+const {
+    // ..,
+    deleteTodo
+} = require('./APIs/todos')
+
+const {
+    // ..,
+    editTodo
+} = require('./APIs/todos')
+
+const {
+    loginUser
+} = require('./APIs/users')
+
+const {
+    // ..,
+    signUpUser
+} = require('./APIs/users')
+
+app.post('/signup', signUpUser);
+
+// Users
+app.post('/login', loginUser);
+
+app.put('/todo/:todoId', editTodo);
+app.delete('/todo/:todoId', deleteTodo);
 app.post('/todo', postOneTodo);
 app.get('/todos', getAllTodos);
 exports.api = functions.https.onRequest(app);
---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyCp0l4GBnipRyX_2sYqBOl7OPzyA_3UpV8",
+    authDomain: "portfolio-leonl.firebaseapp.com",
+    projectId: "portfolio-leonl",
+    storageBucket: "portfolio-leonl.appspot.com",
+    messagingSenderId: "1052286242126",
+    appId: "1:1052286242126:web:0409d6ab7aa1cd5e95e21f",
+    measurementId: "G-Y0GXVCK36Q"
+  };
